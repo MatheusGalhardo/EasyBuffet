d7d5604ab1a2c1849ebc8b9793475dc076717a80
Rafael <rafaelcdmm@gmail.com>
2017-05-19 19:08:24 -0300

    Subindo novo main

---
+++ src/main/webapp/dashboard/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// Texto - Novo Evento
+ $(document).ready(function() {
+    Materialize.updateTextFields();
+  });
